9b189363d6e6bf5954822fd47b37f2d823d071ec
bairon.benitez <ingbaironsis@gmail.com>
2022-11-08 06:45:31 -0500

    Validacion de datos para crear presupuesto

--- routes/budget.route.js
+++ routes/budget.route.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
-const { createBudget } = require('../controller/budget.controller')
+const { createBudget } = require('../controller/budget.controller');
+const { validateErrors } = require('../middlewares/valitadateErros');
 
-router.post('/',createBudget);
+router.post(
+    '/',
+    check('amount').isNumeric().withMessage('El monto debe ser un numero')
+    .isLength({ min: 1 }).withMessage('El presupuesto minimo es 1'),
+    validateErrors,
+    createBudget
+);
 
 module.exports = router;
